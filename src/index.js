1a62b6eb27a58e0e208290075a278500e7b793ff
johanleche <johan@everscape.io>
2019-09-25 20:28:54 +0200

    Task 3 OK - without unit test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-const express        = require('express');
-const headToHeadJSON = require('./data/headtohead');
+const express = require('express');
+const fetch   = require('node-fetch');
 
 const app = express();
 
 const sortDataByContentId = data => data.sort((a, b) => (a.id - b.id));
 
-app.get('/players', (req, res) => res.json(sortDataByContentId(headToHeadJSON.players)));
+const getHeadToHeadFile = async () => {
+  const response = await fetch('https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json');
 
-app.get('/players/:id', (req, res) => {
+  return response.json();
+};
+
+app.get('/players', async (req, res) => {
+  const headToHeadJSON = await getHeadToHeadFile();
+
+  return res.json(sortDataByContentId(headToHeadJSON.players))
+});
+
+app.get('/players/:id', async (req, res) => {
+  const headToHeadJSON = await getHeadToHeadFile();
+
   const playerId = parseInt(req.params.id, 10); // better cast this one ;)
-
-  const player = headToHeadJSON.players.filter(player => player.id === playerId);
+  const player   = headToHeadJSON.players.filter(player => player.id === playerId);
 
   if (player.length === 0) {
     return res.sendStatus(404);
@@ -20,5 +31,5 @@ app.get('/players/:id', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Yellow ~mustard~ - oops - ball players app listening on port ${process.env.PORT || 3000}!`),
+  console.log(`~Yellow mustard~ - oops - yellow ball players app listening on port ${process.env.PORT || 3000}!`),
 );
